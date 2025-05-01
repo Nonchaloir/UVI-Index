9d2265a757228b251787697c2b201c00ac7af7c4
JustL <147842033+Nonchaloir@users.noreply.github.com>
2025-05-01 23:58:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,47 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import bs4
-import re
 import chromedriver_autoinstaller
 
 def main():
-
     chromedriver_autoinstaller.install()
-    op = webdriver.ChromeOptions()
-    op.add_argument('headless')
-    driver = webdriver.Chrome(options=op)
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    
+    driver = webdriver.Chrome(options=options)
     driver.get("https://www.nea.gov.sg/corporate-functions/weather/ultraviolet-index")
-    UVI = driver.find_element(By.XPATH, "//div[contains(@class, 'circle__container')]")
-    Lastest_hour = driver.find_element(By.XPATH, "//span[contains(@class, 'latest-hour')]")
-    Lastest_time = driver.find_element(By.XPATH, "//span[contains(@class, 'latest-date')]")
+
+    wait = WebDriverWait(driver, 10)  # wait up to 10 seconds
+
+    UVI = wait.until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'circle__container')]")))
+    Lastest_hour = wait.until(EC.presence_of_element_located((By.XPATH, "//span[contains(@class, 'latest-hour')]")))
+    Lastest_time = wait.until(EC.presence_of_element_located((By.XPATH, "//span[contains(@class, 'latest-date')]")))
 
     UVI_Info_Website = UVI.text
     Lastest_hour_Info = Lastest_hour.text
     Lastest_time_Info = Lastest_time.text
-    driver.close()
+    driver.quit()
 
     print(UVI_Info_Website)
     print(f'{Lastest_time_Info} {Lastest_hour_Info}:')
 
-
-    # load the file
+    # load and update index.html
     with open("index.html") as inf:
         txt = inf.read()
         soup = bs4.BeautifulSoup(txt, 'html.parser')
 
-    # create new link
-    #new_link = soup.new_tag("link", rel="icon", type="image/png", href="img/tor.png")
-    # insert it into the document
-    #soup.head.append(new_link)
+    UVI_Info = soup.find('div', id="UVI Index")
+    DateTime_Info = soup.find('div', id="DateTime")
 
-    UVI_Info = soup.find('div', id = "UVI Index")
-    DateTime_Info = soup.find('div', id = "DateTime")
+    if UVI_Info:
+        UVI_Info.string = UVI_Info_Website
+    if DateTime_Info:
+        DateTime_Info.string = f'{Lastest_time_Info} {Lastest_hour_Info}:'
 
-    UVI_Info.string = UVI_Info_Website
-    DateTime_Info.string = f'{Lastest_time_Info} {Lastest_hour_Info}:'
-
-
-    # save the file again
     with open("index.html", "w") as outf:
         outf.write(str(soup))
 
